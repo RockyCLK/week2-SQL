3c46f938bee7ae60af40e5098183eafe150a5c34
RockyCLK <92781200+RockyCLK@users.noreply.github.com>
2023-06-29 14:22:15 -0400

    Update queryRentalLog_allCustomers.sql
    changed all "LEFT JOIN" to "JOIN"

--- queryRentalLog_allCustomers.sql
+++ queryRentalLog_allCustomers.sql
@@ -1,7 +1,7 @@
 SELECT c.customer_id, c.first_name, c.last_name, f.title, r.rental_date
 FROM customer c
-LEFT JOIN rental r ON c.customer_id = r.customer_id
-LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
-LEFT JOIN film f ON i.film_id = f.film_id
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
 GROUP BY c.customer_id, c.first_name, c.last_name, f.title, r.rental_date
 ORDER BY c.customer_id ASC, r.rental_date ASC;
