fca76cb38bca79365e9e833d202995b724f6cfce
RockyCLK <92781200+RockyCLK@users.noreply.github.com>
2023-06-30 15:59:56 -0400

    Update queryCustomerListFullNameFullAddress.sql

--- queryCustomerListFullNameFullAddress.sql
+++ queryCustomerListFullNameFullAddress.sql
@@ -1,5 +1,5 @@
 SELECT	
-	concat_ws(' ', c.first_name, c.last_name) AS full_name,
+	concat(c.first_name,' ', c.last_name) AS full_name,
 	concat_ws(', ', a.address, a.address2, a.district, ct.city, a.postal_code, cty.country) AS full_address
 FROM customer c
 JOIN address a ON c.address_id = a.address_id
