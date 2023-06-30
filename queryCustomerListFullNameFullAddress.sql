35d13f116c6119b0840a244be19f646b3814170b
RockyCLK <92781200+RockyCLK@users.noreply.github.com>
2023-06-30 14:41:34 -0400

    Add files via upload

---
+++ queryCustomerListFullNameFullAddress.sql
@@ -0,0 +1,6 @@
+SELECT	concat(c.first_name,' ', c.last_name) AS full_name,
+		concat(a.address,', ', a.address2,', ', a.district,', ', ct.city,', ', cty.country) AS full_address
+FROM customer c
+JOIN address a ON c.address_id = a.address_id
+JOIN city ct ON a.city_id = ct.city_id
+JOIN country cty ON ct.country_id = cty.country_id
