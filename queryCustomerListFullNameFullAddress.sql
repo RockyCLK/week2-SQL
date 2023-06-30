220bce55eeac79fe3d784209cfa2d4770e7af256
RockyCLK <92781200+RockyCLK@users.noreply.github.com>
2023-06-30 15:57:33 -0400

    Add files via upload

--- queryCustomerListFullNameFullAddress.sql
+++ queryCustomerListFullNameFullAddress.sql
@@ -1,5 +1,6 @@
-SELECT	concat(c.first_name,' ', c.last_name) AS full_name,
-	concat(a.address,', ', a.address2,', ', a.district,', ', ct.city,', ', a.postal_code,', ', cty.country) AS full_address
+SELECT	
+	concat_ws(' ', c.first_name, c.last_name) AS full_name,
+	concat_ws(', ', a.address, a.address2, a.district, ct.city, a.postal_code, cty.country) AS full_address
 FROM customer c
 JOIN address a ON c.address_id = a.address_id
 JOIN city ct ON a.city_id = ct.city_id
