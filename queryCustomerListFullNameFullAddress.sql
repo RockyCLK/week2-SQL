286c788f532254eb62b40a95ccb7a4cdf8efd3b0
RockyCLK <92781200+RockyCLK@users.noreply.github.com>
2023-06-30 15:17:40 -0400

    Update queryCustomerListFullNameFullAddress.sql

--- queryCustomerListFullNameFullAddress.sql
+++ queryCustomerListFullNameFullAddress.sql
@@ -1,5 +1,5 @@
 SELECT	concat(c.first_name,' ', c.last_name) AS full_name,
-		concat(a.address,', ', a.address2,', ', a.district,', ', ct.city,', ', cty.country) AS full_address
+	concat(a.address,', ', a.address2,', ', a.district,', ', ct.city,', ', a.postal_code,', ', cty.country) AS full_address
 FROM customer c
 JOIN address a ON c.address_id = a.address_id
 JOIN city ct ON a.city_id = ct.city_id
